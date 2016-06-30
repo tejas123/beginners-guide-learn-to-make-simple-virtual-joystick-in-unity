569e369d7fb02c7fc0a02187a24ac4f8f0737c20
Mukund-iMac <yatin@theappguruz.com>
2016-06-30 16:44:11 +0530

    Code Uploaded

---
+++ Assets/MovePlayers.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayers : MonoBehaviour {
+
+	public float moveSpeed = 05f;
+	public VJHandler jsMovement;
+	
+	private Vector3 direction;
+	private float xMin,xMax,yMin,yMax;
+	
+	void Update () {
+		
+		direction = jsMovement.InputDirection; //InputDirection can be used as per the need of your project
+		
+		if(direction.magnitude != 0){
+		
+			transform.position += direction * moveSpeed;
+			transform.position = new Vector3(Mathf.Clamp(transform.position.x,xMin,xMax),Mathf.Clamp(transform.position.y,yMin,yMax),0f);//to restric movement of player
+		}	
+	}	
+	
+	void Start(){
+	
+		//Initialization of boundaries
+		xMax = Screen.width - 50; // I used 50 because the size of player is 100*100
+		xMin = 50; 
+		yMax = Screen.height - 50;
+		yMin = 50;
+	}
+}
